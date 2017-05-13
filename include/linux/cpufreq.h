6e6213f0402385e859f081a83fe71a854254be74
Joe Maples <joe@frap129.org>
2017-05-12 21:08:01 -0500

    Add Electron
    Signed-off-by: LordShenron <priyanshupandeydelhi@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -528,6 +528,9 @@ extern struct cpufreq_governor cpufreq_gov_blu_active;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELECTRON)
+extern struct cpufreq_governor cpufreq_gov_electron;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_electron)
 #endif
 
 /*********************************************************************
